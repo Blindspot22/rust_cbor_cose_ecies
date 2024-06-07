866bb69c3821df2325491751f8b8bc97a19a9a17
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-07 11:30:38 +0100

    implementing ECIES

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use serde::{Serialize, Deserialize};
 use cose::crypto::{Signer, Verifier};
 use cose::sign::{COSESign1Builder, COSESign1};
 use cose::CborSerializable;
+use ecies::{encrypt, decrypt};
 use rand::rngs::OsRng;
 use rsa::{RSAPrivateKey, RSAPublicKey, PaddingScheme};
 use rsa::pkcs1::ToRsaPrivateKey;
+use secp256k1::{SecretKey, PublicKey};
 use sha2::Sha256;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyData {
+    name: String,
+    value: u32,
+}
 
 fn sign_data(cbor_data: &[u8]) -> Vec<u8> {
     // Generate RSA key pair
@@ -33,6 +42,20 @@ fn verify_data(cose_data: &[u8], public_key: &RSAPublicKey) -> bool {
     sign1.verify(&verifier).is_ok()
 }
 
+fn ecies_encrypt_decrypt(cbor_data: &[u8]) -> Vec<u8> {
+    // Generate a key pair
+    let secret_key = SecretKey::new(&mut rand::thread_rng());
+    let public_key = PublicKey::from_secret_key(&secret_key);
+
+    // Encrypt the CBOR data
+    let encrypted_data = encrypt(&public_key, cbor_data).unwrap();
+
+    // Decrypt the data
+    let decrypted_data = decrypt(&secret_key, &encrypted_data).unwrap();
+
+    decrypted_data
+}
+
 fn main() {
     let data = MyData {
         name: String::from("example"),
@@ -51,4 +74,12 @@ fn main() {
     let public_key = RSAPublicKey::from_pkcs1_der(&cose_data).unwrap();
     let is_valid = verify_data(&cose_data, &public_key);
     println!("Signature valid: {:?}", is_valid);
+
+    // Encrypt and then decrypt the CBOR data using ECIES
+    let decrypted_data = ecies_encrypt_decrypt(&cbor_data);
+    println!("Decrypted Data: {:?}", decrypted_data);
+
+    // Deserialize the decrypted CBOR data
+    let decoded: MyData = serde_cbor::from_slice(&decrypted_data).unwrap();
+    println!("Decoded Data: {:?}", decoded);
 }
