4f9e0a488110605d2f40360501209c62a07142bd
Enow Scott <148756598+Blindspot22@users.noreply.github.com>
2024-06-07 11:16:14 +0100

    implementing CBOR serialization and deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyData {
+    name: String,
+    value: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = MyData {
+        name: String::from("example"),
+        value: 42,
+    };
+
+    // Serialize to CBOR
+    let cbor_data = serde_cbor::to_vec(&data).unwrap();
+    println!("CBOR Data: {:?}", cbor_data);
+
+    // Deserialize from CBOR
+    let decoded: MyData = serde_cbor::from_slice(&cbor_data).unwrap();
+    println!("Decoded Data: {:?}", decoded);
 }
